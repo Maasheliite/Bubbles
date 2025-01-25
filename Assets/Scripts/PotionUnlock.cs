c06378606fd9dfabaffc1bc22c539d44aaf6fc02
Auseklītis <peteris.hermanis@protonmail.com>
2025-01-26 01:41:29 +0200

    added unlockable potions and the logic to unlock them

---
+++ Assets/Scripts/PotionUnlock.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionUnlock : MonoBehaviour
+{
+    public GameObject greyPotion;
+    public bool greyPotionUnlocked;
+    public GameObject whitePotion;
+    public bool whitePotionUnlocked;
+    public GameObject blackPotion;
+    public bool blackPotionUnlocked;
+    public GoalPotion GoalPotion;
+
+    void Start()
+    {
+        greyPotion.SetActive(false);
+        whitePotion.SetActive(false);
+        blackPotion.SetActive(false);
+        GoalPotion = (GoalPotion) GameObject.FindObjectOfType(typeof(GoalPotion));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lowestPercent=10.0f;
+        for(int i=0;i<GoalPotion.score.Count;i++){
+            if(GoalPotion.score[i]<lowestPercent){
+                lowestPercent=GoalPotion.score[i];
+            }
+        }
+        if(lowestPercent<=0.20f){
+            greyPotionUnlocked=true;
+            greyPotion.SetActive(true);
+        }
+        if(lowestPercent<=0.10f){
+            whitePotionUnlocked=true;
+            whitePotion.SetActive(true);
+        }
+        if(lowestPercent<=0.05f){
+            blackPotionUnlocked=true;
+            blackPotion.SetActive(true);
+        }
+    }
+}
